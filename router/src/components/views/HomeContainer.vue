3467508242a6c92742467e03df2ba7d8300985b2
XiChi <474504964@qq.com>
2019-08-10 22:35:35 +0800

    change

--- router/src/components/views/HomeContainer.vue
+++ router/src/components/views/HomeContainer.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>home
-        <div v-for="(item,i) in newsList" :key=i>{{item.text}}</div>
+    <div>
+        <div v-for="(item,i) in newsList" :key=i>{{item.cparagraph}} <br>
+            <img :src="item.image" width="100">
+        </div>
     </div>
 </template>
 
@@ -12,16 +14,26 @@ export default {
         }
     },
     created(){
-        this.getNews()
+        for(var i =0 ; i<3 ; i++){
+              this.getNews();
+        }
     },
     methods:{
         getNews(){
-            this.axios.get("https://api.apiopen.top/getJoke?page=1&count=2&type=video").then((res) => {
+            /*const id = '1'
+             this.axios.get("https://api.apiopen.top/getJoke?page=1&count=2&type=video").then((res) => {
                  console.log(res)
                  if(res.status === 200){
                       this.newsList = res.data.result;
                  }
-                })
+                }) 
+            this.axios.get(`https://easy-mock.com/mock/5d47dc4a7b18701359195de6/example/restful/${id}/list`).then((res)=>{
+                console.log(res)
+            })*/
+            this.axios.get(` https://easy-mock.com/mock/5d4bb1bc80d98b05000cb3b9/test/home`).then((res)=>{
+                console.log(res)
+                this.newsList.push(res.data.data)
+            })
         }
     }
 }
