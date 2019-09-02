776d0dc8a35b21638827e3f11c8230fbe8783668
XiChi <474504964@qq.com>
2019-09-02 23:34:55 +0800

    become green

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <canvasControl/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import canvasControl from './components/canvasControl.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    canvasControl
+  },
 }
 </script>
 
